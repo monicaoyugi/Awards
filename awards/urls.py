3f2c64257dafc55f15e89d8c1aecc72d54fe80fb
monicaoyugi <monicaoyugi@gmail.com>
2020-03-17 19:12:38 +0300

    highlighted out vote post

--- awards/urls.py
+++ awards/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('signup/',views.index,name='signup'),
     path('post/',views.index,name='posts'),
-    path('new/post/',views.new_post, name='new_post'),
+    # path('new_post/',views.new_post, name='new_post'),
     path('profile/', views.profile, name='profile'),
     path('search/',  views.search_results, name='search'),
 ]
